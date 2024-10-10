39baa6b00081b80a920b0b879fa272cb034a9490
Valter B Freitas Jr <valter191@hotmail.com>
2024-10-10 00:13:41 -0300

    feat: validation on pathname

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import Link from "next/link";
 import { Button } from "./ui/button";
+import { LayoutGrid, Package, ShoppingBasketIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
     <div className="w-64 bg-white">
       {/* Image */}
@@ -9,14 +15,32 @@ const Sidebar = () => {
       </div>
       {/* Botões */}
       <div className="flex flex-col gap-2 p-2">
-        <Button variant="ghost" className="justify-start">
-          Dashboard
+        <Button
+          variant={pathname === "/" ? "secondary" : "ghost"}
+          className="justify-start gap-2"
+          asChild
+        >
+          <Link href="/">
+            <LayoutGrid size={18} /> Dashboard
+          </Link>
         </Button>
-        <Button variant="ghost" className="justify-start">
-          Produtos
+        <Button
+          variant={pathname === "/products" ? "secondary" : "ghost"}
+          className="justify-start gap-2"
+          asChild
+        >
+          <Link href="/products">
+            <Package size={18} /> Produtos
+          </Link>
         </Button>
-        <Button variant="ghost" className="justify-start">
-          Vendas
+        <Button
+          variant={pathname === "/sales" ? "secondary" : "ghost"}
+          className="justify-start gap-2"
+          asChild
+        >
+          <Link href="/sales">
+            <ShoppingBasketIcon size={18} /> Vendas
+          </Link>
         </Button>
       </div>
     </div>
