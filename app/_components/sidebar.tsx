6f21fe4d4368b519c9fe59c90fcf418567177844
Valter B Freitas Jr <valter191@hotmail.com>
2024-10-09 23:52:31 -0300

    feat: add button shadcn

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,15 +1,23 @@
+import { Button } from "./ui/button";
+
 const Sidebar = () => {
   return (
     <div className="w-64 bg-white">
       {/* Image */}
       <div>
-        <h1 className="px-8 py-6 font-bold text-2xl">Stockly</h1>
+        <h1 className="px-8 py-6 text-2xl font-bold">Stockly</h1>
       </div>
       {/* Botões */}
       <div className="flex flex-col gap-2 p-2">
-        <button className="bg-yellow-100 px-6 py-3">Dashboard</button>
-        <button className="bg-yellow-200 px-6 py-3">Produtos</button>
-        <button className="bg-yellow-300 px-6 py-3">Vendas</button>
+        <Button variant="ghost" className="justify-start">
+          Dashboard
+        </Button>
+        <Button variant="ghost" className="justify-start">
+          Produtos
+        </Button>
+        <Button variant="ghost" className="justify-start">
+          Vendas
+        </Button>
       </div>
     </div>
   );
