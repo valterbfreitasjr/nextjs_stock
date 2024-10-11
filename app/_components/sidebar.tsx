36135b465795565e85b5bff4f59c10f44a56f63c
Valter B. Freitas Jr <valterbfreitasjr@gmail.com>
2024-10-11 00:52:25 -0300

    feat: add sidebar button component

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,12 +1,7 @@
-"use client";
-
-import Link from "next/link";
-import { Button } from "./ui/button";
 import { LayoutGrid, Package, ShoppingBasketIcon } from "lucide-react";
-import { usePathname } from "next/navigation";
+import SidebarButton from "./sidebar-button";
 
 const Sidebar = () => {
-  const pathname = usePathname();
   return (
     <div className="w-64 bg-white">
       {/* Image */}
@@ -15,33 +10,15 @@ const Sidebar = () => {
       </div>
       {/* Botões */}
       <div className="flex flex-col gap-2 p-2">
-        <Button
-          variant={pathname === "/" ? "secondary" : "ghost"}
-          className="justify-start gap-2"
-          asChild
-        >
-          <Link href="/">
-            <LayoutGrid size={18} /> Dashboard
-          </Link>
-        </Button>
-        <Button
-          variant={pathname === "/products" ? "secondary" : "ghost"}
-          className="justify-start gap-2"
-          asChild
-        >
-          <Link href="/products">
-            <Package size={18} /> Produtos
-          </Link>
-        </Button>
-        <Button
-          variant={pathname === "/sales" ? "secondary" : "ghost"}
-          className="justify-start gap-2"
-          asChild
-        >
-          <Link href="/sales">
-            <ShoppingBasketIcon size={18} /> Vendas
-          </Link>
-        </Button>
+        <SidebarButton href="/">
+          <LayoutGrid size={18} /> Dashboard
+        </SidebarButton>
+        <SidebarButton href="/products">
+          <Package size={18} /> Produtos
+        </SidebarButton>
+        <SidebarButton href="/sales">
+          <ShoppingBasketIcon size={18} /> Vendas
+        </SidebarButton>
       </div>
     </div>
   );
