56b617606dfb82afcf5c0df86a319151044074da
Valter B Freitas Jr <valter191@hotmail.com>
2024-10-16 00:52:20 -0300

    feat: add table products

---
+++ app/products/_components/table-columns.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { CircleIcon } from "lucide-react";
+
+const getStatusLabel = (status: string) => {
+  if (status === "IN_STOCK") {
+    return "Em estoque";
+  }
+  return "Fora do estoque";
+};
+
+export const productTableColumns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Produto",
+  },
+  {
+    accessorKey: "price",
+    header: "Valor unitário",
+  },
+  {
+    accessorKey: "stock",
+    header: "Estoque",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: (row) => {
+      const product = row.row.original;
+      const label = getStatusLabel(product.status);
+      return (
+        <Badge
+          variant={label === "Em estoque" ? "default" : "destructive"}
+          className="items-center gap-2"
+        >
+          <CircleIcon
+            size={12}
+            className={`${label && "fill-destructive-foreground"}`}
+          />
+          {label}
+        </Badge>
+      );
+    },
+  },
+];
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,30 @@
-const ProductsPage = () => {
-  return <h1>Products Page</h1>;
+import { PlusIcon } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { productTableColumns } from "./_components/table-columns";
+
+const ProductsPage = async () => {
+  const products = await db.product.findMany({});
+
+  return (
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+      <div className="flex w-full items-end justify-between">
+        {/* ESQUERDA */}
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de produtos
+          </span>
+          <h2 className="text-xl font-semibold">Produtos</h2>
+        </div>
+        {/* DIREITA */}
+        <Button className="gap-2">
+          <PlusIcon size={20} /> Novo produto
+        </Button>
+      </div>
+      <DataTable columns={productTableColumns} data={products} />
+    </div>
+  );
 };
 
 export default ProductsPage;
