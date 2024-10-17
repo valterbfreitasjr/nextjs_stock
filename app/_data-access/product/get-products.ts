bcff1390357f23e680c067fa0e6d20bf1a7c5a91
Valter B Freitas Jr <valter191@hotmail.com>
2024-10-16 23:22:50 -0300

    refactor: add data access layer

---
+++ app/_data-access/product/get-products.ts
@@ -0,0 +1,7 @@
+import "server-only";
+import { db } from "@/app/_lib/prisma";
+import { Product } from "@prisma/client";
+
+export const getProducts = async (): Promise<Product[]> => {
+  return await db.product.findMany({});
+};
